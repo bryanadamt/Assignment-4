f1e8dc07ed2b8d22e9ace592457c1694f654ebb2
bryanadamt <bryanat@uw.edu>
2021-05-30 21:03:41 -0700

    unfinished factory

---
+++ Factories/movieFactory.h
@@ -0,0 +1,29 @@
+// ------------------------------------------------ movieFactory.h -------------------------------------------------------
+// Bryan Adam Tjendra, CSS 343B
+// Created: 30/5/2021
+// Date of Last Modification:
+// --------------------------------------------------------------------------------------------------------------------
+// Purpose - class used to make different genres of movie
+// --------------------------------------------------------------------------------------------------------------------
+
+#ifndef MOVIEFACTORY_H
+#define MOVIEFACTORY_H
+
+#include "../Movies/classics.h"
+#include "../Movies/comedy.h"
+#include "../Movies/drama.h"
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+class MovieFactory {
+    public:
+        // Read the entire movie file
+        void readMovieData(ifstream&);
+
+        // Assign movie that was read from the file to one of the genres
+        bool movieMaker(char, ifstream&)
+}
+
+#endif
