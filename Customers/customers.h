b1f7056d0f5e03828a92464adebd057fc76c750b
bryanadamt <bryanat@uw.edu>
2021-05-30 18:23:58 -0700

    incomplete customers

---
+++ Customers/customers.h
@@ -0,0 +1,28 @@
+// ------------------------------------------------ customers.h -------------------------------------------------------
+// Bryan Adam Tjendra, CSS 343B
+// Created: 30/5/2021
+// Date of Last Modification:
+// --------------------------------------------------------------------------------------------------------------------
+// Purpose - This class is a representation of a customer
+// --------------------------------------------------------------------------------------------------------------------
+
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+#include "../Data Structures/bstree.h"
+
+using namespace std;
+
+class Customers {
+    public:
+
+    private:
+        // Unique Id Number
+        int customerID;
+        // first name
+        string firstN;
+        // last name
+        string lastN;
+}
+
+#endif
