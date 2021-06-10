a2e818f9addf859253a70eed9dab75499d9c749e
bryanadamt <bryanat@uw.edu>
2021-06-10 15:42:08 -0700

    ndefined reference to

---
+++ Movie_Store/store.cpp
@@ -0,0 +1,42 @@
+// ------------------------------------------------ store.cpp -------------------------------------------------------
+// Bryan Adam Tjendra, CSS 343B
+// Created: 30/5/2021
+// Date of Last Modification:
+// --------------------------------------------------------------------------------------------------------------------
+// Purpose - This class is to represent the movie rental store 
+// It combines all other classes together
+// --------------------------------------------------------------------------------------------------------------------
+
+#include "store.h"
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+//---------------------------- readMovieData(ifstream&) -------------------------------------
+// Read the entire movie file
+void Store::readMovieData(ifstream& movieData) {
+    if (!movieData) {
+		cout << "File could not be opened." << endl;
+		return;
+	}
+
+    string line;
+    while (getline(movieData, line)) {
+        Movies *newMovie = NULL;
+        stringstream ss(line);
+        newMovie = mFactory.movieMaker(ss);
+
+        if (newMovie == NULL) {
+            continue;
+        }
+
+        if (newMovie->getGenre() == 'F') {
+            comedy.insert(newMovie);
+        } else if (newMovie->getGenre() == 'D') {
+            drama.insert(newMovie);
+        } else {
+            classics.insert(newMovie);
+        }
+    }
+}
